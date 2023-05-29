27891d42cc30eeb2e68ed8325edba21dd7d6fd1f
Italo Costa <italocc16@gmail.com>
2023-05-29 11:56:58 -0300

    refactor: installing dependencies and refactoring code

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
-
+import 'dotenv/config'
+import { carsRoutes } from './routes/cars'
+import { logsRoutes } from './routes/logs'
 
 const app = fastify()
 
+app.register(carsRoutes)
+app.register(logsRoutes)
 
 app.listen({
     port: 3333,
